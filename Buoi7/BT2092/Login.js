ac11d9b40a686b6148e5df9a2f4b1690e9f52ff5
Thunguyenk1 <thunguyenhtay@gmail.com>
2023-10-26 15:03:15 +0700

    add readme into the project

---
+++ Buoi7/BT2092/Login.js
@@ -0,0 +1,14 @@
+
+
+function successData() {
+    usernameTag = document.getElementById('userame_id')
+    passwordTag = document.getElementById('password_id')
+
+    alert('Đăng nhập thành công')
+}
+
+function resetData() {
+    usernameTag.value = ""
+    passwordTag.value = ""
+
+}
